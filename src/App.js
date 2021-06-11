88b3fdf835d68f9fd6932ebb474542f7007e0617
Dinh Q. Le <lqdquangdinh@gmail.com>
2021-06-11 17:37:18 +0700

    somewhat done with about

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from "react";
 import "./App.css";
 
 import Background from "./components/Background";
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
     <div className="App">
       <Background />
-      <section className="container--flex">
+      <section className="container--flex text-center">
         <h1 className="title">Hi, I'm Dinh</h1>
         <p className="subtitle">I make beautiful things that people enjoy</p>
       </section>
+      <AboutMe id="about-me" />
       <section className="container--flex">
-        <h1>About me</h1>
+        <h1>Featured project</h1>
       </section>
+      <section className="container--flex">
+        <h1>Contact</h1>
+      </section>
+      <footer className="footer text-center">
+        <span>Made by Dinh Q. Le, 2021</span>
+      </footer>
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <section className="container--flex">
+      <div className="about-me">
+        <div className="about-me__left">
+          <img
+            className="about-me__img"
+            src={`${process.env.PUBLIC_URL}/Dinh.jpg`}
+            alt="Dinh in Vungtau city"
+          />
+        </div>
+        <div className="about-me__right">
+          <h1 className="about-me__header">About me</h1>
+          <p className="paragraph">
+            My name is Dinh Q. Le. I was a YouTuber and video editor. After we
+            got the YouTube Gold Play button, I decided to change my career
+            path. I become a developer so I can continue making beautiful things
+            many people enjoy.
+          </p>
+          <button className="btn mt-1">
+            <span class="material-icons">description</span>CV
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
